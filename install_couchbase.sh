fe95525c69c323025327cbb7578b7113b9a9b517
Tim M. Hidalgo <tim.hidalgo@gmail.com>
2014-08-26 15:30:07 -0700

    Initial commit
    Uploaded install_couchbase.sh

---
+++ install_couchbase.sh
@@ -0,0 +1,14 @@
+wget http://packages.couchbase.com/releases/2.2.0/couchbase-server-community_2.2.0_x86_64.deb
+
+dpkg -i couchbase-server-community_2.2.0_x86_64.deb
+
+ufw enable
+
+ufw allow 11209:11211/tcp
+ufw allow 4369
+ufw allow 8091:8092/tcp
+ufw allow 21100:21299/tcp
+
+firefox localhost:8091 &
+
+
